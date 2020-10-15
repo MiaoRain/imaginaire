3b7ba1d6a350571d5f93817247d3c0e85b6f2cc2
MiaoRain <zhangmiaotju@gmail.com>
2020-10-15 10:59:58 +0800

    first adjustment

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2020 NVIDIA Corporation.  All rights reserved.
-#
+# miao1015
 # This work is made available under the Nvidia Source Code License-NC.
 # To view a copy of this license, check out LICENSE.md
 import argparse
